098dfc8f353bc3bb4a3717bad42ffdff1bc2bd1f
mirmrose <45744112+mirmrose@users.noreply.github.com>
2018-12-09 19:25:18 -0500

    Initial commit

---
+++ LinkedList.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+
+public static class LinkedListExtensions
+{
+    public static bool RemoveAt(int index, LinkedList<object> List1)
+    {
+        //set current index and node to 0, and first node
+        int currentIndex = 0;
+        LinkedListNode<object> currentNode = List1.First;
+
+        while (currentNode != null)
+        {
+
+            //remove if current index matches passed in index, and end function returning true
+            if (currentIndex == index)
+            {
+                List1.Remove(currentNode);
+                return true;
+            }
+
+            //set currentnode to next in collection, set currentindex to increment by one
+            currentNode = currentNode.Next;
+            currentIndex += 1;
+        }
+        //return false if never matched passed in index
+        return false;
+    }
+    public static bool InsertAt(int index, object o, LinkedList<object> List1)
+    {
+
+    //set current index and node to 0, and first node
+        int currentIndex = 0;
+        LinkedListNode<object> currentNode = List1.First;
+
+        while (currentNode != null)
+        {
+
+            //remove if current index matches passed in index, and end function returning true
+            if (currentIndex == index)
+            {
+                List1.AddBefore(currentNode,o);
+                return true;
+            }
+
+            //set currentnode to next in collection, set currentindex to increment by one
+            currentNode = currentNode.Next;
+            currentIndex += 1;
+        }
+        //return false if never matched passed in index
+        return false;
+    }
+    public static void PrintReverse(LinkedList<object> List1)
+    {
+        LinkedListNode<object> currentNode = List1.Last;
+        string Results = null;
+        while (currentNode != null)
+        {
+            Results += currentNode.Value;
+            currentNode = currentNode.Previous;
+
+        }
+
+        System.Console.WriteLine(Results);
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			{
+				LinkedList<object> list1 = new LinkedList<object>();
+				list1.AddLast("A");
+				list1.AddLast("B");
+				list1.AddLast("C");
+				list1.AddLast("D");
+				list1.AddLast("E");
+				list1.AddLast("F");
+				list1.AddLast("G");
+				list1.AddLast("H");
+				list1.AddLast("I");
+				list1.AddLast("J");
+
+				int index = 2;
+
+                //var retVal = LinkedListExtensions.RemoveAt(index, list1);
+                object o = "New Val";
+                var retVal = LinkedListExtensions.InsertAt(index, o,list1);
+
+                Console.WriteLine(retVal);
+
+				foreach (var s in list1)
+					Console.WriteLine(s);
+
+                LinkedListExtensions.PrintReverse(list1);
+			}
+
+		}
+	}
+}
